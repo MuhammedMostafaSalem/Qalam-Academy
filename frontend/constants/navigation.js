305e99f2abee6077be1c18e9db39707b332518cd
Marwan Salem <marwansalem164@gmail.com>
2026-06-28 20:00:42 +0300

    feat(frontend): initialize project foundation

---
+++ frontend/constants/navigation.js
@@ -0,0 +1,42 @@
+export const NAVIGATION = [
+    {
+        id: 1,
+        title: "Home",
+        href: "/",
+    },
+    {
+        id: 2,
+        title: "About",
+        href: "/about",
+    },
+    {
+        id: 3,
+        title: "Services",
+        href: "/services",
+    },
+    {
+        id: 4,
+        title: "Courses",
+        href: "/courses",
+    },
+    {
+        id: 5,
+        title: "Portfolio",
+        href: "/portfolio",
+    },
+    {
+        id: 6,
+        title: "Store",
+        href: "/store",
+    },
+    {
+        id: 7,
+        title: "Blog",
+        href: "/blog",
+    },
+    {
+        id: 8,
+        title: "Contact",
+        href: "/contact",
+    },
+];
