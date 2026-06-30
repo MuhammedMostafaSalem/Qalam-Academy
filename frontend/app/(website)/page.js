a79edaea281a14b366af482c3fac01e987ba4488
Marwan Salem <marwansalem164@gmail.com>
2026-06-30 05:04:29 +0300

    Add services section

--- frontend/app/(website)/page.js
+++ frontend/app/(website)/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/home/hero/Hero";
+import Services from "@/components/home/services/Services";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 }
