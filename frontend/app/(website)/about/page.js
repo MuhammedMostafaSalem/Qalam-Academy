bf7d86cb43e816b75abb96c273fe9cf4454fecc9
Marwan Salem <marwansalem164@gmail.com>
2026-07-01 09:17:52 +0300

    Add hero and story sections

---
+++ frontend/app/(website)/about/page.js
@@ -0,0 +1,11 @@
+import AboutHero from "@/components/about/hero/AboutHero";
+import StorySection from "@/components/about/story/StorySection";
+
+export default function About() {
+    return (
+        <>
+            <AboutHero />
+            <StorySection />
+        </>
+    );
+}
