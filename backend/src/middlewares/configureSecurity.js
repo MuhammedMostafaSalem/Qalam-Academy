55bd893c69bb24f60e84060e220639162d12ff5b
Marwan Salem <marwansalem164@gmail.com>
2026-07-19 02:31:54 +0300

    Delete express-mongo-sanitize and xss-clean package

--- backend/src/middlewares/configureSecurity.js
+++ backend/src/middlewares/configureSecurity.js
@@ -1,8 +1,6 @@
 const helmet = require("helmet");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
 const hpp = require("hpp");
 const compression = require("compression");
 const env = require("../config/env");
@@ -21,8 +19,6 @@ const configureSecurity = (app) => {
             max: 100,
         })
     );
-    app.use(mongoSanitize());
-    app.use(xss());
     app.use(hpp());
     app.use(compression());
 }
