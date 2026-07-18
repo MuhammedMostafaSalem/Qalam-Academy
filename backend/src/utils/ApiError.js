8f04fac2bc8535ecaaa3d882f409633e6f3aa8a5
Marwan Salem <marwansalem164@gmail.com>
2026-07-19 02:41:12 +0300

    Add errors

--- backend/src/utils/ApiError.js
+++ backend/src/utils/ApiError.js
@@ -1,9 +1,12 @@
 class ApiError extends Error {
-    constructor(message, statusCode) {
+    constructor(message, statusCode, errors = null) {
         super(message);
 
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+
+        this.errors = errors;
+
         this.isOperational = true;
 
         Error.captureStackTrace(this, this.constructor);
