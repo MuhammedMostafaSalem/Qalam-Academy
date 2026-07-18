df6826394f8c278f19606c9a8cb99c2e55d5bff3
Marwan Salem <marwansalem164@gmail.com>
2026-07-19 02:40:56 +0300

    Update user modle

--- backend/src/modules/users/user.model.js
+++ backend/src/modules/users/user.model.js
@@ -2,51 +2,27 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const env = require("../../config/env");
-const validator = require("validator");
 
 // User Schema Definition
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: [true, "Username is required"],
-        minlength: [2, "Username must be at least 2 characters"],
-        maxlength: [30, "Username must be at most 30 characters"],
+        required: true,
+        unique: true,
         trim: true,
         lowercase: true
     },
     email: {
         type: String,
-        required: [true, "Email is required"],
+        required: true,
         unique: true,
         trim: true,
         lowercase: true,
-        validate: {
-            validator: validator.isEmail,
-            message: "Please enter a valid email address",
-        },
-    },
-    phone: {
-        type: String,
-        required: [true, "Phone number is required"],
-        unique: true,
-        trim: true,
-        validate: {
-            validator: (value) => /^\+?[1-9]\d{9,14}$/.test(value),
-            message: "Please enter a valid phone number in international format.",
-        },
     },
     password: {
         type: String,
-        required: [true, "Password is required"],
-        minlength: [6, "Password must be at least 6 characters"],
-        maxlength: [20, "Password must be at most 20 characters"],
+        required: true,
         select: false,  // Exclude password field by default when querying
-        validate: {
-            validator: function (value) {
-                return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&_-]{6,20}$/.test(value);
-            },
-            message: "Password must be 6-20 characters, contain at least one letter and one number, and may include symbols: _@$!%*#?&-"
-        }
     },
     avatar: {
         type: String,
@@ -82,15 +58,16 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving the user
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    // only hash the password if it has been modified (or is new)
+    if (!this.isModified("password")) return;
 
+    // generate a salt
     try {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
-        next();
     } catch (err) {
-        next(err);
+        console.log(err)
     }
 });
 
