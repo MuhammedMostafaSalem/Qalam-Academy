4945799a1422a629599bb758bd064955c90187fa
Marwan Salem <marwansalem164@gmail.com>
2026-07-19 02:38:18 +0300

    Add middleware validation

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -9,21 +9,52 @@ const {
     forgotPassword,
     resetPassword
 } = require("./auth.controller");
+const validate = require("../../middlewares/validate");
+const signupSchema = require("./validators/signup.schema");
+const verifyOtpSchema = require("./validators/verifyOtp.schema");
+const resendOtpSchema = require("./validators/resendOtp.schema");
+const loginSchema = require("./validators/login.schema");
+const forgetSchema = require("./validators/forgetPass.schema");
+const resetSchema = require("./validators/resetPass.schema");
 
 const router = express.Router();
 
 // Authentication
-router.post("/signup", signup);
-router.post("/login", login);
+router.post(
+    "/signup",
+    validate(signupSchema),
+    signup
+);
+router.post(
+    "/login",
+    validate(loginSchema),
+    login
+);
 router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
 
 // OTP
-router.post("/verify-otp", verifyAccountOtp);
-router.post("/resend-otp", resendAccountOtp);
+router.post(
+    "/verify-otp",
+    validate(verifyOtpSchema),
+    verifyAccountOtp
+);
+router.post(
+    "/resend-otp",
+    validate(resendOtpSchema),
+    resendAccountOtp
+);
 
 // Password
-router.post("/forgot-password", forgotPassword);
-router.patch("/reset-password", resetPassword);
+router.post(
+    "/forgot-password",
+    validate(forgetSchema),
+    forgotPassword
+);
+router.patch(
+    "/reset-password",
+    validate(resetSchema),
+    resetPassword
+);
 
 module.exports = router;
