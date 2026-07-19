8618060336905bad3424a5d9b855bd40404ad07a
Marwan Salem <marwansalem164@gmail.com>
2026-07-19 07:02:54 +0300

    Add serviceRoutes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const authRoutes = require("../modules/auth/auth.routes");
+const serviceRoutes = require("../modules/services/service.routes");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/services", serviceRoutes);
 
 module.exports = router;
