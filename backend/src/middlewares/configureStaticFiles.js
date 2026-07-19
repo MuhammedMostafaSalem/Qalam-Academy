6c8b14dc43235fb8f9e4beed16b0325e53f028bf
Marwan Salem <marwansalem164@gmail.com>
2026-07-19 07:01:59 +0300

    Edit static files path

--- backend/src/middlewares/configureStaticFiles.js
+++ backend/src/middlewares/configureStaticFiles.js
@@ -4,7 +4,7 @@ const express = require("express");
 const configureStaticFiles = (app) => {
     app.use(
         "/uploads",
-        express.static(path.join(__dirname, "uploads"))
+        express.static(path.join(__dirname, "..", "uploads"))
     );
 }
 
