adf75f954c1e9d7b134ee55bfa69857dbd6e92fd
Marwan Salem <marwansalem164@gmail.com>
2026-07-31 11:45:19 +0300

    Add payment and checkout routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -14,8 +14,6 @@ const couponRoutes = require("../modules/coupon/coupon.routes");
 const cartRoutes = require("../modules/cart/cart.routes");
 const orderRoutes = require("../modules/order/orders.routes");
 const enrollmentRoutes = require("../modules/enrollment/enrollment.routes");
-const checkoutRoutes = require("../modules/checkout/checkout.routes");
-const paymentRoutes = require("../modules/payment/payment.routes");
 
 const router = express.Router();
 
@@ -33,7 +31,5 @@ router.use("/coupons", couponRoutes);
 router.use("/cart", cartRoutes);
 router.use("/orders", orderRoutes);
 router.use("/enrollments", enrollmentRoutes);
-router.use("/checkout", checkoutRoutes);
-router.use("/payment", paymentRoutes);
 
 module.exports = router;
