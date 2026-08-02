0bf305999d5dc07135ac632089cf9dc6f99668e7
Marwan Salem <marwansalem164@gmail.com>
2026-08-02 19:27:11 +0300

    Create Language Middleware

---
+++ backend/src/middlewares/language.js
@@ -0,0 +1,26 @@
+const {
+    SUPPORTED_LANGUAGES,
+    DEFAULT_LANGUAGE,
+} = require("../config/languages");
+
+module.exports = (req, res, next) => {
+    let lang =
+        req.headers["accept-language"] ||
+        req.headers["Accept-language"] ||
+        req.query.lang ||
+        DEFAULT_LANGUAGE;
+
+    if (lang.includes(",")) {
+        lang = lang.split(",")[0];
+    }
+
+    lang = lang.toLowerCase().trim();
+
+    if (!SUPPORTED_LANGUAGES.includes(lang)) {
+        lang = DEFAULT_LANGUAGE;
+    }
+
+    req.language = lang;
+
+    next();
+};
