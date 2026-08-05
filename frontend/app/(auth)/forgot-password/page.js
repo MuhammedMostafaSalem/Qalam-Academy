67a630da8001832998ac3df1894100ea91446a34
Marwan Salem <marwansalem164@gmail.com>
2026-08-05 04:39:34 +0300

    Create forget password page

---
+++ frontend/app/(auth)/forgot-password/page.js
@@ -0,0 +1,9 @@
+import ForgetPasswordLayout from "@/components/layout/auth/ForgetPasswordLayout";
+
+const ForgotPasswordForm = () => {
+    return (
+        <ForgetPasswordLayout />
+    )
+};
+
+export default ForgotPasswordForm;
