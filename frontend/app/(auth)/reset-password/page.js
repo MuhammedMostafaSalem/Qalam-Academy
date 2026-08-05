1c2ec4ea8adf4a0e415ff4c5b1e0abe57039005e
Marwan Salem <marwansalem164@gmail.com>
2026-08-05 04:39:16 +0300

    Create reset password page

---
+++ frontend/app/(auth)/reset-password/page.js
@@ -0,0 +1,17 @@
+import ResetPasswordForm from "@/components/auth/resetPassword/ResetPasswordForm";
+
+export default function ResetPasswordPage() {
+    return (
+        <main
+            className="
+                flex
+                min-h-screen
+                items-center
+                justify-center
+                px-4
+            "
+        >
+            <ResetPasswordForm />
+        </main>
+    );
+}
