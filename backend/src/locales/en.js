6f5ddfda6334f86b335da092fb0bb5b8073792ed
Marwan Salem <marwansalem164@gmail.com>
2026-08-05 04:46:44 +0300

    Add fields and auth

--- backend/src/locales/en.js
+++ backend/src/locales/en.js
@@ -3,12 +3,15 @@ module.exports = {
         success: "Success",
         failed: "Something went wrong",
         notFound: "Resource not found",
+        duplicate: "The {{field}} '{{value}}' already exists",
     },
 
     course: {
+        fetched: "Course fetched successfully",
         created: "Course created successfully",
         updated: "Course updated successfully",
         deleted: "Course deleted successfully",
+        notFound: "Course not found",
     },
 
     hero: {
@@ -16,4 +19,25 @@ module.exports = {
         updated: "Hero updated successfully",
         deleted: "Hero deleted successfully",
     },
+
+    fields: {
+        email: "Email",
+        title: "Title",
+        slug: "Slug",
+    },
+
+    auth: {
+        sentOtp: "otp sent to your email for verification",
+        verifiedEmail: "User successfully verified",
+        verifiedPassword: "OTP verified, you can reset your password now",
+        resendEmail: "Verification OTP resent successfully",
+        resendPassword: "Password reset OTP resent successfully",
+        notmatchPassword: "Passwords do not match",
+        notFound: "User not found",
+        successMessage: "Reset password successfully",
+        invalidResetToken: "Invalid or expired reset token please send your email again",
+        verifyFisrt: "Please verify your account first",
+        incorrectPass: "Password is incorrect",
+        successLogin: "Logged in successfully"
+    }
 };
