8143f3640df58fd06c9920903ad2f6e021280fc6
Marwan Salem <marwansalem164@gmail.com>
2026-08-08 15:52:07 +0300

    Add CategoryLayout component

--- frontend/app/(dashboard)/dashboard/categories/page.js
+++ frontend/app/(dashboard)/dashboard/categories/page.js
@@ -1,26 +1,7 @@
-import CategoriesTable from "@/components/dashboard/categories/CategoriesTable";
-import CategoriesToolbar from "@/components/dashboard/categories/CategoriesToolbar";
-import PageHeader from "@/components/dashboard/PageHeader";
+import CategoryLayout from "@/components/layout/dashboard/categoryLayout";
 
 export default function AdminCategories() {
     return (
-        <div
-            className="
-                glass 
-                rounded-3xl
-                border
-                border-border
-                p-6
-                shadow-sm
-            "
-        >
-            <PageHeader
-                title="تصنيفات الكورسات"
-                description="ادارة جميع تصنيفات كورسات المنصة"
-                button="اضافة تصنيف جديدة"
-            />
-            <CategoriesToolbar />
-            <CategoriesTable />
-        </div>
+        <CategoryLayout />
     )
 }
