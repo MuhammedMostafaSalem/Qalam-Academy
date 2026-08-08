f2dc69d4ff001c7d95023338f820f5c10f00caa5
Marwan Salem <marwansalem164@gmail.com>
2026-08-08 15:44:07 +0300

    Remove authReducer slice

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import toastReducer from "./slices/toastSlice"
-import authReducer from "./slices/authSlice"
 
 export const store = configureStore({
     reducer: {
         toast: toastReducer,
-        auth: authReducer,
     },
 });
