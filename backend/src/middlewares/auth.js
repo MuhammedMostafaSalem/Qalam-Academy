aee325385c358f91cf97f6745cfc1f3c963b0b0a
Marwan Salem <marwansalem164@gmail.com>
2026-08-10 20:12:55 +0300

    Update auth middleware to work with Qalam_Token

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -8,24 +8,41 @@ const User = require("../modules/users/user.model");
 // Middleware for verifying that the user is logged in (authentication)
 const isAuthenticatedUser = catchAsync(async (req, res, next) => {
     let token;
-    const authHeader = req.headers["Authorization"] || req.headers["authorization"];
+    const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    // Check Authorization header
-    if (authHeader && authHeader.startsWith("Bearer "))
-        token = authHeader.split(" ")[1];
+    // // Check Authorization header
+    // if (authHeader && authHeader.startsWith("Bearer "))
+    //     token = authHeader.split(" ")[1];
+
+    // 1. Try Cookie first
+    if (req.cookies?.Qalam_Token) {
+        token = req.cookies.Qalam_Token;
+    }
+
+    // 2. Fallback to Authorization header
+    if (!token) {
+        if (authHeader?.startsWith("Bearer ")) {
+            token = authHeader.split(" ")[1];
+        }
+    }
 
     // If there are no tokens
     if (!token)
-        return next(new ApiError("Unauthorized. Please login to access this resource.", StatusCodes.UNAUTHORIZED));
+        return next(new ApiError(req.t("auth.PleaseLoginToAccess"), StatusCodes.UNAUTHORIZED));
 
     // Verify token(errors handled by global error middleware)
-    const decoded = jwt.verify(token, env.jwtAccessSecret);
+    const decoded = jwt.verify(token, env.jwtSecretToken);
 
     // Get user from database
     const user = await User.findById(decoded.id);
 
     // If user not found
-    if (!user) return next(new ApiError("User not found", StatusCodes.NOT_FOUND));
+    if (!user) return next(new ApiError(req.t("auth.notFound"), StatusCodes.NOT_FOUND));
+
+    // If user active === false 
+    if (!user.isActive) {
+        return next(new ApiError(req.t("user.administratorDeactivatedAccount"),StatusCodes.FORBIDDEN));
+    }
 
     // Attach user to request
     req.user = user;
@@ -40,7 +57,10 @@ const authorizeRoles = (...roles) => {
         if (!roles.includes(req.user.role)) {
             return next(
                 new ApiError(
-                    `Role (${req.user.role}) is not allowed to access this resource`,
+                    // `Role (${req.user.role}) is not allowed to access this resource`,
+                    req.t("auth.roleNotAllowed", {
+                        role: req.user.role,
+                    }),
                     StatusCodes.FORBIDDEN
                 )
             );
