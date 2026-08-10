0d020dce89d31a273f4f48cb40e8a35f6545f1f7
Marwan Salem <marwansalem164@gmail.com>
2026-08-10 20:19:37 +0300

    Add AdminUsersLayout component

--- frontend/app/(dashboard)/dashboard/users/page.js
+++ frontend/app/(dashboard)/dashboard/users/page.js
@@ -1,26 +1,7 @@
-import PageHeader from "@/components/dashboard/PageHeader";
-import UsersTable from "@/components/dashboard/users/UsersTable";
-import UsersToolbar from "@/components/dashboard/users/UsersToolbar";
+import AdminUsersLayout from "@/components/layout/dashboard/AdminUsersLayout";
 
 export default function AdminUsers () {
     return (
-        <div
-            className="
-                glass 
-                rounded-3xl
-                border
-                border-border
-                p-6
-                shadow-sm
-            "
-        >
-            <PageHeader
-                title="المستخدمين"
-                description="ادارة جميع المستخدمين الذين لديهم صلاحية الوصول الى لوحة التحكم"
-                button="اضافة مستخدم جديدة"
-            />
-            <UsersToolbar />
-            <UsersTable />
-        </div>
+        <AdminUsersLayout />
     )
 }
