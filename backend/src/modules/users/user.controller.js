dcd06ac300a8bff7db9dc40693241c09abde3381
Marwan Salem <marwansalem164@gmail.com>
2026-08-10 20:15:46 +0300

    Create getAllUsersByAdmin controller

--- backend/src/modules/users/user.controller.js
+++ backend/src/modules/users/user.controller.js
@@ -9,10 +9,11 @@ const {
 } = require("../../utils/crudFactory");
 const User = require("./user.model");
 const sendResponse = require("../../utils/sendResponse");
+const ApiFeatures = require("../../utils/ApiFeatures");
 
-// Get all users
+// Public: Get all users
 exports.getUsers = getAll(User, {
-    modelName: "Users",
+    modelName: "user",
     searchFields: [
         "firstName",
         "lastName",
@@ -23,14 +24,55 @@ exports.getUsers = getAll(User, {
     defaultSort: "-createdAt",
 });
 
+// Admin: Get All Users Except Current Admin
+exports.getAllUsersByAdmin = catchAsync(async (req, res) => {
+    const features = new ApiFeatures(User, req.query)
+        .search([
+            "firstName",
+            "lastName",
+            "email",
+            "role",
+        ])
+        .filter()
+        .sort("-createdAt")
+        .loadMore(10);
+
+    // Exclude the currently logged-in admin
+    features.query = features.query.find({
+        _id: {
+            $ne: req.user.id,
+        },
+    });
+
+    // Admin can see more information,
+    // but still don't expose password/OTP/reset tokens.
+    features.query = features.query.select(
+        "firstName lastName slug email avatar role isActive createdAt"
+    );
+
+    await features.buildMeta();
+
+    const users = await features.query;
+
+    return sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: req.t("user.fetched"),
+        data: {
+            users,
+        },
+        meta: features.meta,
+    });
+});
+
 // Get one user
 exports.getUser = getOne(User, {
-    modelName: "User",
+    modelName: "user",
 });
 
 // Update user
 exports.updateUser = updateOne(User, {
-    modelName: "User",
+    modelName: "user",
     fileFields: ["avatar"],
 });
 
@@ -40,25 +82,30 @@ exports.changePassword = catchAsync(async (req, res, next) => {
     const { currentPassword, newPassword, confirmPassword } = req.body;
 
     // User can only change his own password
-    if (req.user.id !== req.params.id)
-        next(new ApiError("You are not allowed to change another user's password", StatusCodes.FORBIDDEN));
+    if (req.user.id !== req.params.id) {
+        return next(new ApiError(req.t("user.notAllowed"), StatusCodes.FORBIDDEN));
+    }
 
     const user = await User.findById(req.params.id).select("+password");
 
-    if (!user)
-        next(new ApiError("User not found", StatusCodes.NOT_FOUND));
+    if (!user) {
+        return next(new ApiError(req.t("user.notFound"), StatusCodes.NOT_FOUND));
+    }
 
     const isPasswordMatched = await user.comparePassword(currentPassword);
 
-    if (!isPasswordMatched)
-        next(new ApiError("Current password is incorrect", StatusCodes.BAD_REQUEST));
+    if (!isPasswordMatched) {
+        return next(new ApiError(req.t("user.incorrectCurrentPassword"), StatusCodes.BAD_REQUEST));
+    }
 
-    if (newPassword !== confirmPassword)
-        next(new ApiError("Password confirmation does not match", StatusCodes.BAD_REQUEST));
+    if (newPassword !== confirmPassword) {
+        return next(new ApiError(req.t("user.notMatchComfirmPassword"), StatusCodes.BAD_REQUEST));
+    }
 
     // Optional (Recommended)
-    if (currentPassword === newPassword)
-        next(new ApiError("New password must be different from current password", StatusCodes.BAD_REQUEST));
+    if (currentPassword === newPassword) {
+        return next(new ApiError(req.t("user.newPasswordDifferent"), StatusCodes.BAD_REQUEST));
+    }
 
     user.password = newPassword;
 
@@ -67,17 +114,54 @@ exports.changePassword = catchAsync(async (req, res, next) => {
     return sendResponse(res, {
         statusCode: StatusCodes.OK,
         success: true,
-        message: "Password changed successfully.",
+        message: req.t("user.successMessagePassword"),
         data: null,
     });
 });
 
 // Delete user
 exports.deleteUser = deleteOne(User, {
-    modelName: "User",
+    modelName: "user",
     fileFields: ["avatar"],
 });
 
+// Update user from admin for role and isActive
+exports.updateUserByAdmin = catchAsync(async (req, res, next) => {
+    const { role, isActive } = req.body;
+
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+        return next(new ApiError(req.t("user.notFound"), StatusCodes.NOT_FOUND));
+    }
+
+    if (req.params.id === req.user.id && isActive === false) {
+        return next(new ApiError(req.t("user.cannotDeactivate"), StatusCodes.BAD_REQUEST));
+    }
+
+    if (req.params.id === req.user.id && role && role !== "admin") {
+        return next(new ApiError(req.t("user.cannotChangeOwnRole"), StatusCodes.BAD_REQUEST));
+    }
+
+    if (role !== undefined) {
+        user.role = role;
+    }
+
+    if (isActive !== undefined) {
+        user.isActive = isActive;
+    }
+
+    await user.save();
+
+    return sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: req.t("user.updated"),
+        data: {
+            user,
+        },
+    });
+});
 
 // Get Theme Mode
 exports.getThemeMode = catchAsync(async (req, res) => {
@@ -86,7 +170,7 @@ exports.getThemeMode = catchAsync(async (req, res) => {
     return sendResponse(res, {
         success: true,
         statusCode: StatusCodes.OK,
-        message: "Theme mode fetched successfully",
+        message: req.t("user.fetchedThemeMode"),
         data: {
             themeMode: user.themeMode,
         },
@@ -109,7 +193,7 @@ exports.toggleThemeMode = catchAsync(async (req, res) => {
     return sendResponse(res, {
         success: true,
         statusCode: StatusCodes.OK,
-        message: "Theme mode updated successfully",
+        message: req.t("user.updatedThemeMode"),
         data: {
             themeMode: user.themeMode,
         },
@@ -122,7 +206,7 @@ exports.getCurrentUser = catchAsync(async (req, res, next) => {
     const user = await User.findById(req.user.id).select("-password");
 
     if (!user) {
-        return next(new ApiError(req.t(`auth.notFound`), StatusCodes.NOT_FOUND));
+        return next(new ApiError(req.t(`user.notFound`), StatusCodes.NOT_FOUND));
     }
 
     // Send success response
