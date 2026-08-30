de1705f24036312c688ce132704edec897edf266
Marwan Salem <marwansalem164@gmail.com>
2026-08-31 01:19:04 +0300

    Replase file pages

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -2,16 +2,18 @@ import ProfileHeader from "@/components/dashboard/profile/ProfileHeader";
 import PasswordCard from "@/components/profile/PasswordCard";
 import PersonalInfoCard from "@/components/profile/PersonalInfoCard";
 import ProfileHeroCard from "@/components/profile/ProfileHeroCard";
+import Breadcrumb from "@/components/ui/Breadcrumb";
 
 export default function AdminProfile () {
     return (
-        <>
+        <div className="p-[20px] flex flex-col gap-4">
+            <Breadcrumb />
             {/* <ProfileHeader /> */}
             <ProfileHeroCard />
             {/* تعديل البيانات الشخصية */}
             <PersonalInfoCard />
             {/* مسؤولة عن تغيير كلمة المرور فقط */}
             <PasswordCard />
-        </>
+        </div>
     )
 }
